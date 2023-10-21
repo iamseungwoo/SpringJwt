a457a1d1b399ed62ad3c72f754f8020d7eacd63b
seungwoo byeon <mongoose@yu.ac.kr>
2023-10-21 11:02:19 +0000

    feat: jwt end

---
+++ jjwt/src/main/java/inflearn/jjwt/service/UserService.java
@@ -0,0 +1,55 @@
+package inflearn.jjwt.service;
+
+import java.util.Collections;
+import java.util.Optional;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import inflearn.jjwt.dto.UserDto;
+import inflearn.jjwt.entity.Authority;
+import inflearn.jjwt.entity.User;
+import inflearn.jjwt.repository.UserRepository;
+import inflearn.jjwt.util.SecurityUtil;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public User signup(UserDto userDto) {
+
+        if (userRepository.findOneWithAuthoritiesByUsername(userDto.getUsername()).orElse(null) != null) {
+            throw new RuntimeException("이미 가입되어 있는 유저입니다.");
+        }
+
+        Authority authority = Authority.builder()
+                .authorityName("ROLE_USER")
+                .build();
+
+        User user = User.builder()
+                .username(userDto.getUsername())
+                .password(passwordEncoder.encode(userDto.getPassword()))
+                .nickname(userDto.getNickname())
+                .authorities(Collections.singleton(authority))
+                .activated(true)
+                .build();
+
+        return userRepository.save(user);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<User> getUserWithAuthorites(String username) {
+        return userRepository.findOneWithAuthoritiesByUsername(username);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<User> getMyUserWithAuthorities() {
+        return SecurityUtil.getCurrentUsername().flatMap(userRepository::findOneWithAuthoritiesByUsername);
+    }
+}
